8def6179419c01930161fc7e3b4412f474f19c33
Pacha Saheb <80810225+pachasaheb55@users.noreply.github.com>
2024-06-19 23:08:35 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = "mysql+mysqlconnector://admin:Tempx123@tempxapp.cxhasgxzlsj1.us-east-2.rds.amazonaws.com/tempx"
+URL_DATABASE = ""
 
 engine = create_engine(URL_DATABASE, pool_size= 10, max_overflow= 30)
 
